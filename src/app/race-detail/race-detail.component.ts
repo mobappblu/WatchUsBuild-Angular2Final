6f9a6d07fe7c37662f027115fc297a80d460f90e
brenton ford <brenton.j.ford@gmail.com>
2017-03-19 18:23:08 -0400

    generate component RaceDetails

---
+++ src/app/race-detail/race-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-race-detail',
+  templateUrl: './race-detail.component.html',
+  styleUrls: ['./race-detail.component.css']
+})
+export class RaceDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
