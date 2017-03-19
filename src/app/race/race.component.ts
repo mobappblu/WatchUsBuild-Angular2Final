3d025f8b3a0f7ff93a1390e071d7e66bf5baca1c
brenton ford <brenton.j.ford@gmail.com>
2017-03-19 17:47:09 -0400

    input data

--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'race',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./race.component.css']
 })
 export class RaceComponent implements OnInit {
+
+  @Input() race;
 
   constructor() { }
 
