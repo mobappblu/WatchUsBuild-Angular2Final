299ca208105dc2b3c209dd6f0d4621cfa8eeaf28
brenton ford <brenton.j.ford@gmail.com>
2017-03-19 18:22:40 -0400

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RacesComponent } from './races/races.component';
 import { RaceService } from './races/race.service';
 import { RaceComponent } from './race/race.component';
+import { RaceDetailComponent } from './race-detail/race-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RacesComponent,
-    RaceComponent
+    RaceComponent,
+    RaceDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: RacesComponent }
+    ])
   ],
   providers: [ RaceService ],
   bootstrap: [AppComponent]
